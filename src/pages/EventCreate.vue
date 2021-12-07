20850bfeda334cc16f3cf342782784d3164752d6
mortaen <kolbe.mor@gmail.com>
2021-12-07 15:59:51 +0100

    Implement vuex

--- src/pages/EventCreate.vue
+++ src/pages/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>Id: {{ user.id }}</p>
+    <p>There are {{ catLength }} categories.</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories']),
+  },
+}
+</script>
+
+<style scoped></style>
